29d433d3119a9556eb598c2086075bf6dfa5baf2
faithwest <faithchepkoech425@gmail.com>
2023-09-29 14:34:55 +0300

    complete

---
+++ index.js
@@ -0,0 +1,26 @@
+/*let nums = ['1','2','3','4']
+ console.log(letters[1]);
+ console.log(letters[3]);
+ console.log(letters[2]);
+ console.log(letters[o]);
+
+//pojo:plain old javascript object
+let country = {
+     continent : 'africa',
+     city : 'Nairobi',
+     wonder: 'bigFive',
+}
+console.log(country.city);
+console.log(country['city']);*/
+
+
+//mutate
+let nums = ['1','2','3','4']
+nums[3] = '2'
+nums[1] = 'true'
+nums[0] = '1'
+console.log(nums)
+
+country.city = 'Marakech'
+country['wonder'] = 'beaches'
+console.log(country['wonder'])
